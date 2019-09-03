0a081b92a19c7b584c5f6cc6872b72ea237d7bdc
Tony Johnson <tonyj@stanford.edu>
2019-09-03 16:24:43 -0700

    Improve version formatting

--- src/main/java/org/lsst/ccs/daq/ims/Version.java
+++ src/main/java/org/lsst/ccs/daq/ims/Version.java
@@ -1,24 +1,32 @@
 package org.lsst.ccs.daq.ims;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 /**
+ * Representation of a DAQ version.
  *
  * @author tonyj
  */
 public class Version {
+
+    private final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            .withZone(ZoneId.of("UTC"));
     private final String tag;
-    private final long buildTime;
+    private final Instant buildTime;
     private final boolean dirty;
-    private final int hash;    
- 
-    Version(String tag, long buildTime, boolean dirty, int hash) {
+    private final int hash;
+
+    Version(String tag, long timestampNanos, boolean dirty, int hash) {
         this.tag = tag;
-        this.buildTime = buildTime;
+        this.buildTime = Instant.ofEpochSecond(timestampNanos / 1_000_000_000, timestampNanos % 1_000_000_000);
         this.dirty = dirty;
         this.hash = hash;
     }
 
     @Override
     public String toString() {
-        return "Version{" + "tag=" + tag + ", buildTime=" + buildTime + ", dirty=" + dirty + ", hash=" + hash + '}';
+        return String.format("%s %s(%s,%x)", tag, DATE_TIME_FORMATTER.format(buildTime), dirty ? "dirty" : "", hash);
     }
 }
