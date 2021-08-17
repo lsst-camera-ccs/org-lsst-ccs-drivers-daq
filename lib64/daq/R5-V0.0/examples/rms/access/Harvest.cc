daaa3ff605c46905abdb1759a71460598558e57e
Anthony S. Johnson <tonyj@lsst-dc01.slac.stanford.edu>
2021-08-17 16:22:41 -0700

    Update to DAQv5.0 final

---
+++ lib64/daq/R5-V0.0/examples/rms/access/Harvest.cc
@@ -0,0 +1,85 @@
+
+#include <stdio.h>
+
+#include "Harvest.hh"
+
+#include "rms/Errors.hh"
+#include "rms/Instruction.hh"
+
+using namespace RMS;
+
+/*
+** ++
+**
+**
+** --
+*/
+
+static const char ERROR[]         = "%4s:        ERROR: %s (%i)\n";
+static const char DISPLAY[]       = "%4s:%06x  %08x\n";
+static const char DISPLAY_FAULT[] = "%4s:%06x  %08x - FAULT %s (%i)\n";
+
+void Access::Harvest::process(const DAQ::Location& location, const InstructionList& result, int32_t error)
+{
+  char source[8];
+  location.encode(source);
+
+  if(error) {
+    ++_response_errors;
+    printf(ERROR, source, Errors::decode(error), error); 
+    return;
+  }
+
+  _sources |= DAQ::LocationSet(location);
+  
+  for (unsigned i=0; i < result.level(); ++i)
+  {
+    const Instruction* instruction = result.lookup(i);
+    
+    unsigned   reg           = instruction->reg();
+    uint32_t   operand       = instruction->operand(); 
+    Instruction::Fault fault = instruction->fault();
+    const char* explain      = instruction->decodeFault();
+
+    if(!fault) {
+      ++_instructions; 
+      printf(DISPLAY, source, reg, operand);
+    }
+    else {
+      ++_instruction_errors; 
+      printf(DISPLAY_FAULT, source, reg, operand, explain, fault);
+    }
+  }
+  
+  return;
+}
+
+/*
+** ++
+**
+**
+** --
+*/
+
+void Access::Harvest::clear()
+{
+  _response_errors    = 0;
+  _instructions       = 0;
+  _instruction_errors = 0;
+}
+
+/*
+** ++
+**
+**
+** --
+*/
+
+static const char TOTAL[] = 
+  " Total Sources      = %d, Source Errors      = %d\n"
+  " Total Instructions = %d, Instruction Errors = %d\n";
+
+void Access::Harvest::summary()
+{
+  printf(TOTAL, _sources.numof(), _response_errors, _instructions, _instruction_errors);
+}
