ecd410b9cebf6a640d933f54fb5898d6e5d42a18
Tony Johnson <tonyj@stanford.edu>
2022-02-14 16:38:42 -0800

    Add guider tool

---
+++ src/main/java/org/lsst/ccs/daq/ims/example/GuiderTool.java
@@ -0,0 +1,94 @@
+package org.lsst.ccs.daq.ims.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.lsst.ccs.command.annotations.Argument;
+import org.lsst.ccs.command.annotations.Command;
+import org.lsst.ccs.daq.ims.DAQException;
+import org.lsst.ccs.daq.ims.Guider;
+import org.lsst.ccs.daq.ims.Guider.GuiderROIs;
+import org.lsst.ccs.daq.ims.Guider.ROILocation;
+import org.lsst.ccs.daq.ims.Store;
+import org.lsst.ccs.daq.ims.Version;
+import org.lsst.ccs.utilities.location.Location;
+import org.lsst.ccs.utilities.location.LocationSet;
+
+/**
+ *
+ * @author tonyj
+ */
+public class GuiderTool {
+
+    private static final Logger LOG = Logger.getLogger(GuiderTool.class.getName());
+
+    private Store store;
+    private Guider guider;
+    
+    public GuiderTool() {
+    }
+
+    @Command(name = "connect", description = "Connect to a DAQ guider")
+    public void connect(
+            @Argument(name = "partition", description = "Partition name") String partition) throws DAQException {
+            
+        if (store != null) {
+            store.close();
+        }
+        store = new Store(partition);
+        guider = store.getGuider();
+    }
+
+    @Command(name = "close", description = "Close DAQ store")
+    public void close() throws DAQException {
+        if (store != null) {
+            store.close();
+            store = null;
+        }
+    }
+
+    @Command(name = "stop", description = "Stop the guider")
+    public void stop() throws DAQException {
+        checkStore();
+        guider.stop();
+    }
+
+    @Command(name = "pause", description = "Stop the guider")
+    public void pause() throws DAQException {
+        checkStore();
+        guider.pause();
+    }
+    
+    @Command(name = "stop", description = "Stop the guider")
+    public void resume() throws DAQException {
+        checkStore();
+        guider.resume();
+    }
+    
+        @Command(name = "stop", description = "Stop the guider")
+    public void start() throws DAQException {
+        checkStore();
+        List<ROILocation> locations = new ArrayList<>();
+        Location R22 = Location.of("R22/Reb0");
+        locations.add(new ROILocation(R22, 0, 4, 100, 100));
+        GuiderROIs roi = new GuiderROIs(50, 50, 100, 1, locations);
+        guider.start(roi);
+    }
+    
+    @Command(name = "version", description = "Get version info")
+    public Version version() throws DAQException {
+        return Store.getClientVersion();
+    }
+    
+    @Command(name = "locations", description = "List configured locations")
+    public LocationSet locations() throws DAQException {
+        checkStore();
+        return store.getConfiguredSources();
+    }
+    
+    private void checkStore() {
+        if (store == null) {
+            throw new RuntimeException("Please connect to store first");
+        }
+    }
+}
---
+++ src/main/resources/guider.app
@@ -0,0 +1,4 @@
+org.lsst.ccs.application.mainClass=org.lsst.ccs.shell.JLineShell
+org.lsst.ccs.application.args=--prompt ">> " --dictionaryClasses org.lsst.ccs.daq.ims.example.GuiderTool
+org.lsst.ccs.application.description=Interact with DAQ guider
+
