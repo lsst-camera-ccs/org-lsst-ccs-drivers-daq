9cb16f91ba916f5c5ae6127dbc696b42f31fc960
Tony Johnson <tonyj@stanford.edu>
2023-05-09 13:53:59 -0700

    Add missing getters on Series

--- src/main/java/org/lsst/ccs/daq/guider/Series.java
+++ src/main/java/org/lsst/ccs/daq/guider/Series.java
@@ -25,6 +25,30 @@ public class Series {
         this.remaining = remaining;
     }
 
+    public Status getStatus() {
+        return status;
+    }
+
+    public Instant getBegin() {
+        return begin;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public int getStamps() {
+        return stamps;
+    }
+
+    public List<SensorLocation> getConfigured() {
+        return configured;
+    }
+
+    public List<SensorLocation> getRemaining() {
+        return remaining;
+    }
+
     @Override
     public String toString() {
         return "Series{" + "status=" + status + ", begin=" + begin + ", sequence=" + sequence + ", stamps=" + stamps + ", configured=" + configured + ", remaining=" + remaining + '}';
